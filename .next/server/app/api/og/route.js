1a6eff17eb4fb4515955e27edda69677325277e7
praveen.mariappasamy <praveen.mariappasamy@zohocorp.com>
2025-01-07 13:08:30 +0530

    initial commit

---
+++ .next/server/app/api/og/route.js
@@ -0,0 +1,8 @@
+const CHUNK_PUBLIC_PATH = "server/app/api/og/route.js";
+const runtime = require("../../../chunks/[turbopack]_runtime.js");
+runtime.loadChunk("server/chunks/node_modules_next_50ab76._.js");
+runtime.loadChunk("server/chunks/[root of the server]__637481._.js");
+runtime.loadChunk("server/chunks/[externals]_next_dist_compiled_@vercel_og_index_node_1f96b6.js");
+runtime.loadChunk("server/chunks/_876e22._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/og/route/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/og/route.tsx [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"eh5wzWCB+nSxuVR1S9gm2U/K8VtPAUm/jrraM5Q8fSI=\"\n}"
